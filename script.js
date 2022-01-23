bef041660051b8d0531c791e66e5b03d53fecf37
dvd83130 <casanova.83130@gmail.com>
2022-01-23 21:16:55 +0100

    adding memory card learning spanish

---
+++ script.js
@@ -0,0 +1,26 @@
+function maFonction01() {
+    var div = document.getElementById("espagnol");
+    if (div.style.display === "none") {
+      div.style.display = "block";
+    } else {
+      div.style.display = "none";
+    }
+  }
+  
+  function maFonction02() {
+    var div = document.getElementById("francais");
+    if (div.style.display === "none") {
+      div.style.display = "block";
+    } else {
+      div.style.display = "none";
+    }
+  }
+  
+  function maFonction03() {
+    var div = document.getElementById("exemple");
+    if (div.style.display === "none") {
+      div.style.display = "block";
+    } else {
+      div.style.display = "none";
+    }
+  }
